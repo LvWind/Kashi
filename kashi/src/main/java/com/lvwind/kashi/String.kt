e83ad29d8bb2771095b24071221dad35d663590b
LvWind <lvwind.shaw@gmail.com>
2017-09-04 17:47:02 +0800

    update

---
+++ kashi/src/main/java/com/lvwind/kashi/String.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017. Jason Shaw <lvwind.shaw@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lvwind.kashi
+
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+
+/**
+ * post the string in toast
+ */
+fun String.toast() = toast(this, false)
+
+/**
+ * get md5
+ */
+fun String.md5() = encrypt(this, "MD5")
+
+/**
+ * get sha-1
+ */
+fun String.sha1() = encrypt(this, "SHA-1")
+
+
+private fun encrypt(string: String?, type: String): String {
+    if (string.isNullOrEmpty()) {
+        return ""
+    }
+    val md5: MessageDigest
+    return try {
+        md5 = MessageDigest.getInstance(type)
+        val bytes = md5.digest(string!!.toByteArray())
+        bytes2Hex(bytes)
+    } catch (e: NoSuchAlgorithmException) {
+        ""
+    }
+}
+
+private fun bytes2Hex(bts: ByteArray): String {
+    var des = ""
+    var tmp: String
+    for (i in bts.indices) {
+        tmp = Integer.toHexString(bts[i].toInt() and 0xFF)
+        if (tmp.length == 1) {
+            des += "0"
+        }
+        des += tmp
+    }
+    return des
+}
